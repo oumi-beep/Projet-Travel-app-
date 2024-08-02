60364db924f5f4679c3a4775dc85038478515c38
oumi-beep <117539799+oumi-beep@users.noreply.github.com>
2024-08-02 17:00:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/travel_app_projet/"
+  base: "/travel_app_projet/",
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
